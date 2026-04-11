eb74e8107e23ae7094cfbe6aa1625e0fdf7577f9
BrianFellDownTheWell <123607721+BrianFellDownTheWell@users.noreply.github.com>
2026-04-11 03:22:41 -0700

    Added basic player movement for dodging the hazards
    Started on a minigame where the player's hot air balloon must dodge hazards, by implementing controls via the left/right arrows and A/D respectively.

--- Assets/Scripts/MoveBalloon.cs
+++ Assets/Scripts/MoveBalloon.cs
@@ -14,11 +14,11 @@ public class MoveBalloon : MonoBehaviour
     void Update()
     {
         // Use left and right arrows, or the A and D keys to move
-        if (Input.GetKey(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+        if (Input.GetKey(KeyCode.A) || Input.GetKeyKeyCode.LeftArrow)){
             velocity = new Vector2(-1.0f, 0.0f) * Time.fixedDeltaTime * speed;
             rb.MovePosition(rb.position + velocity);
         }
-        if (Input.GetKey(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
             velocity = new Vector2(1.0f, 0.0f) * Time.fixedDeltaTime * speed;
             rb.MovePosition(rb.position + velocity);
         }
