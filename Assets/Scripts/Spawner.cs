0e4f3f2e2d278d260cc2b54f232b55d9360ebf30
BrianFellDownTheWell <123607721+BrianFellDownTheWell@users.noreply.github.com>
2026-04-13 16:22:05 -0700

    Added textures to the player and falling objects in the dodge objects minigame

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour
 {
 
+    public List<Sprite> hazardSprites;
     public GameObject hazard;
     public float spawnInterval;
     public float xRange = 8f; // how far left/right hazards can spawn
@@ -32,6 +34,18 @@ public class Spawner : MonoBehaviour
         float randomX = Random.Range(-xRange, xRange);
         Vector3 spawnPosition = new Vector3(randomX, transform.position.y + 10f, 0);
         Debug.Log("Spawn triggered at: " + Time.time);
-        Instantiate(hazard, spawnPosition, Quaternion.identity);
+        // The instantiated hazard instance
+        GameObject hazardInst = Instantiate(hazard, spawnPosition, Quaternion.identity);
+        // Assign the instance to one of the possible sprite types
+        if (hazardSprites.Count > 0)
+        {
+            SpriteRenderer sr = hazardInst.GetComponent<SpriteRenderer>();
+            if (sr != null) 
+            { 
+                int spriteNum = Random.Range(0, hazardSprites.Count);
+                sr.sprite = hazardSprites[spriteNum];
+                sr.transform.localScale = new Vector3(0.2f, 0.2f, 1f);
+            }
+        }
     }
 }
